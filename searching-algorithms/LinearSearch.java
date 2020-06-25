83d08e0b7c5fe1332a9ce99d5a0d7aaf5a676c60
Suvrat jain <33749832+Suvrat31@users.noreply.github.com>
2020-06-25 22:44:52 +0530

    Add files via upload

---
+++ searching-algorithms/LinearSearch.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class LinearSearch{
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        Scanner scanner = new Scanner(System.in);
+        int value;
+        System.out.println("Enter an array of 10 numbers to be sorted using linear search :");
+    //user input for array values
+        for(int i=0; i<10; i++){
+        value = scanner.nextInt();
+        arr[i] = value;
+    }
+    System.out.println("Enter the value to be searched :");
+    int searchVal = scanner.nextInt();
+    //sending the array to a sorting func
+    int valueAt = search(arr ,searchVal);
+   
+    System.out.println("The value you are searching for is at " + (valueAt+1) +" position" );
+    }
+
+    private static int search(int arr[], int searchVal) {
+        int arrlen = arr.length; 
+        for(int z=0; z<arrlen; z++){
+            if(arr[z]==searchVal){
+                return z;
+            }
+        }return 0;
+    }
+}
