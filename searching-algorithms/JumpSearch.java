65229ed76cbd4f7b89a378345d06640aab0b05ca
Suvrat jain <33749832+Suvrat31@users.noreply.github.com>
2020-07-01 22:56:35 +0530

    Add files via upload

---
+++ searching-algorithms/JumpSearch.java
@@ -0,0 +1,53 @@
+// searches in a Sorted Array, Hence Sort the array first before implementing. Using Recursive Binary Search...
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class JumpSearch {
+    public static void main(String[] args) {
+        int value;
+        int[] arr = new int[10];
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter a sorted array of 10 numbers to be searched using jump search :");
+    //user input for array values
+        for(int i=0; i<10; i++){
+        value = scanner.nextInt();
+        arr[i] = value;
+    }
+    System.out.println("Enter the value to be searched :");
+    int searchVal = scanner.nextInt();
+    //sending the array to a searching func
+    int valueAt = search(arr  ,searchVal);
+    scanner.close();
+    System.out.println("The value you are searching for is at " + (valueAt+1) +" position" );
+    }
+   
+   
+    private static int search(int arr[],  int searchVal) {
+        //length of the array
+        int value = 0;
+        int arrlen = arr.length;
+
+        //size to be jumped during traversal 
+        int jumpSize = (int)Math.floor(Math.sqrt(arrlen));
+
+        while (arr[jumpSize] <= searchVal && jumpSize < arrlen) {
+            value = jumpSize;
+            jumpSize = jumpSize + (int)Math.floor(Math.sqrt(arrlen));
+
+            if(jumpSize > arrlen -1)
+            return -1;
+        }
+
+        //performing linear search
+        for(int a =value; a<jumpSize; a++){
+            if(arr[a]==searchVal)
+                return a;
+        }
+       
+        return 0;
+    }
+
+}
